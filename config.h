73d8a6cd801450617a9d407bba1008836c3e7a6e
thesheriffofnottingham <47672967+thesheriffofnottingham@users.noreply.github.com>
2019-04-05 12:25:55 -0700

    Add files via upload

--- config.h
+++ config.h
@@ -64,6 +64,10 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 	{ datetime, "[%s]",           "%F %T" },
-	{ battery_state, "[CHR %s]",  "BAT0"  },
-	{ battery_perc, "[BAT %2s%%]","BAT0"  },
+	{ run_command, "[VOL %s%%]", "/bin/sh -c \"amixer get Master | tail -n1 | grep -Po '\\[\\K[^%]*' | head -n1\"" },
+	{ battery_state, "[CHR %s]",   "BAT0" },
+	{ battery_perc, "[BAT %2s%% | ", "BAT0" },
+	{ battery_remaining, "%2s]",  "BAT0" },
+	{ wifi_perc, "[%3s%% on ",    "wlan0" },
+	{ wifi_essid, "%s]",          "wlan0" },
 };
